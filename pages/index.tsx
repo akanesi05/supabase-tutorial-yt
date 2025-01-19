8213e70f79957ab61b901ba257a5aa4aba1804d9
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-19 17:18:50 +0900

    一覧で表示させる

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import TodoApp from "../components/TodoApp";
+import TodoList from "../components/TodoList";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -14,7 +15,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section><TodoApp/>
+      <section className="flex justify-center items-center h-screen">
+        <TodoApp/>
       </section>
       
     </>
