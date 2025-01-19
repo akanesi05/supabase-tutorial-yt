e6d3240e48c88f62a6ed5d9cd80be97feef4511a
mitsuiakane <akane.sumitomo@gmail.com>
2025-01-19 18:19:11 +0900

    追加削除

--- components/TodoApp.tsx
+++ components/TodoApp.tsx
@@ -1,9 +1,11 @@
 
-import { getAllTodos } from "../utils/supabaseFunctions";
+//import { getAllTodos } from "../utils/supabaseFunctions";
 import TodoList from "../components/TodoList";
 import React, { useEffect, useState } from "react";
+import { addTodo, getAllTodos } from "../utils/supabaseFunctions";
 const TodoApp = () => {
     const [todos, setTodos] = useState<any>([]);
+    const [title, setTitle] = useState<string>("");
 
   
     useEffect(() => {
@@ -14,14 +16,28 @@ const TodoApp = () => {
       };
       getTodos();
     }, []);
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+    
+        if (title === "") return;
+    
+        //Todoの追加
+        await addTodo(title);
+        let todos = await getAllTodos();
+        // setTodos(todos);
+    
+        // setTitle("");
+      };
+    
   return (
     <section className="text-center mb-2 text-2xl font-medium">
         <h3>supabase.todoapp</h3>
-        <form>
-           <input type="text" className="mr-2 shadow-lg p-1 outline-none"/>
+        <form onSubmit={(e) => handleSubmit(e)}>
+           <input type="text" onChange={(e) => setTitle(e.target.value)} value={title} className="mr-2 shadow-lg p-1 outline-none"/>
            <button className="shadow-md border-2 px-1 py-1 rounded-lg bg-green-200">Add</button> 
         </form>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} setTodos={setTodos}/>
     </section>
   )
 }
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
 
-type Props ={
-    todos:Todo[];
-}
+import React from "react";
+import { Todo } from "../utils/interface";
+import { deleteTodo, getAllTodos } from "../utils/supabaseFunctions";
 
-const TodoList = (props:Props) => {
-    const { todos } = props;
+type Props = {
+  todos: Todo[];
+  //setTodos: React.Dispatch<any>;
+  setTodos: React.Dispatch<any>;
+};
+
+const TodoList = (props: Props) => {
+  const { todos, setTodos } = props;
+
+  const handleDelete = async (id: number) => {
+    await deleteTodo(id);
+    let todos = await getAllTodos();
+    setTodos(todos);
+  };
 
   return (
-    <div><ul className="mx-auto">
-        {todos.map((todo) => (<div key={todo.id} className="flex bg-orange-200 rounded-md mt-2 mb-2 p-2 justify-between"><li className="font-medium">☑️{todo.title}</li>
-        <span className="cursor-pointer">❌</span>
-        </div>))}
-       
-        </ul></div>
-    
-  )
-}
+    <div>
+      <ul className="mx-auto">
+        {todos.map((todo) => (
+          <div
+            key={todo.id}
+            className="flex bg-orange-200 rounded-md mt-2 mb-2 p-2 justify-between"
+          >
+            <li className="font-medium">✅ {todo.title}</li>
+            <span
+              className="cursor-pointer"
+              onClick={() => handleDelete(todo.id)}
+            >
+              ✖
+            </span>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default TodoList
+export default TodoList;
